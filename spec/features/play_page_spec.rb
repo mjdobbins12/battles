52d576c496c2e0caa12596627941d403c777b1a6
Mark Dobbins <mjdobbins12@gmail.com>
2020-01-16 20:38:12 +0000

    hit points displayed

--- spec/features/play_page_spec.rb
+++ spec/features/play_page_spec.rb
@@ -1,9 +1,11 @@
 feature 'entering player names' do
 	scenario 'player names are displayed after filling in the form' do
 		visit('/')
-		fill_in :p1, with: 'Frank'
-		fill_in :p2, with: 'Mahmoud'
+		fill_in :p1, with: "Frank"
+		fill_in :p2, with: "Mahmoud"
 		click_button 'Submit'
-		expect(page).to have_content('Frank vs Mahmoud')
+		expect(page).to have_content("Frank vs Mahmoud")
+		expect(page).to have_content("Frank's hit points: 100")
+		expect(page).to have_content("Mahmoud's hit points: 100")
 	end
 end
